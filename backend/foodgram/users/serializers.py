9f684a24b19702cf2a910e951171d7b02bbb685a
loren166 <107641713+loren166@users.noreply.github.com>
2023-05-27 22:45:06 +0300

    workflow

--- backend/foodgram/users/serializers.py
+++ backend/foodgram/users/serializers.py
@@ -1,7 +1,7 @@
+from api.models import Recipe
 from django.contrib.auth import get_user_model
 from djoser.serializers import UserCreateSerializer, UserSerializer
 from rest_framework import serializers
-from api.models import Recipe
 from users.models import Follow
 
 User = get_user_model()
