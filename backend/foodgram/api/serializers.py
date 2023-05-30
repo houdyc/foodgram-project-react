69ce23a1a367f136b287d3b39fb4dc5e17dd242d
loren166 <107641713+loren166@users.noreply.github.com>
2023-05-30 03:46:57 +0300

    image

--- backend/foodgram/api/serializers.py
+++ backend/foodgram/api/serializers.py
@@ -1,6 +1,6 @@
 from drf_base64.fields import Base64ImageField
 from rest_framework import serializers
-from rest_framework.fields import IntegerField, SerializerMethodField
+from rest_framework.fields import IntegerField
 from rest_framework.relations import PrimaryKeyRelatedField
 from users.serializers import CustomUserSerializer
 
