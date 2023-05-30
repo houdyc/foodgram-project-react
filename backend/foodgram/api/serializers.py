7199515b7b774e8166a70864a8203ff702f43f2f
loren166 <107641713+loren166@users.noreply.github.com>
2023-05-30 17:51:51 +0300

    image

--- backend/foodgram/api/serializers.py
+++ backend/foodgram/api/serializers.py
@@ -24,7 +24,6 @@ class RecipeSerializer(serializers.ModelSerializer):
     image = Base64ImageField()
     author = CustomUserSerializer(default=serializers.CurrentUserDefault())
     ingredients = IngredientSerializer(many=True)
-    tags = TagSerializer()
     is_favorited = serializers.SerializerMethodField(
         method_name='get_is_favorited')
     is_in_shopping_cart = serializers.SerializerMethodField(
