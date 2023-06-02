fc30bb6c1c2b067e1fb3f36d0e0075243f8040a1
loren166 <107641713+loren166@users.noreply.github.com>
2023-06-02 05:13:26 +0300

    favorite

--- backend/foodgram/api/views.py
+++ backend/foodgram/api/views.py
@@ -78,7 +78,7 @@ class RecipeViewSet(viewsets.ModelViewSet):
     )
     def download_shopping_cart(self, request):
         ingredients = IngredientRecipe.objects.filter(
-            recipe__shop_list__user=self.request.user
+            recipe__shopping_cart__user=request.user
         ).values(
             'ingredient__name',
             'ingredient__measurement_unit'
