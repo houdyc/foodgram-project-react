0b0f3e154047397a8142a6d57a93ad61424d41c8
loren166 <107641713+loren166@users.noreply.github.com>
2023-06-03 20:07:36 +0300

    123

--- backend/foodgram/api/serializers.py
+++ backend/foodgram/api/serializers.py
@@ -113,7 +113,7 @@ class RecipeWriteSerializer(serializers.ModelSerializer):
         IngredientRecipe.objects.bulk_create(
             [IngredientRecipe(
                 recipe=recipe,
-                ingredient=ingredient['id'],
+                ingredient=Ingredient('id'),
                 amount=ingredient['amount']
             ) for ingredient in ingredients]
         )
