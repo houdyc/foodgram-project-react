fbc15737584c5565dfed993850315f303bbf7fa8
loren166 <107641713+loren166@users.noreply.github.com>
2023-06-05 22:32:59 +0300

    123

--- backend/foodgram/api/serializers.py
+++ backend/foodgram/api/serializers.py
@@ -67,7 +67,7 @@ class RecipeIngredientsSerializer(serializers.ModelSerializer):
 class IngredientWriteSerializer(serializers.ModelSerializer):
     recipe = serializers.PrimaryKeyRelatedField(read_only=True)
     id = serializers.PrimaryKeyRelatedField(
-        source='ingredient',
+        source='ingredient.id',
         queryset=Ingredient.objects.all()
     )
     amount = serializers.IntegerField(write_only=True, min_value=1)
