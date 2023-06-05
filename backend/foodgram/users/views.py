51221abb39d64d86d9b41930285c292f75767c98
loren166 <107641713+loren166@users.noreply.github.com>
2023-06-06 02:07:15 +0300

    pravka

--- backend/foodgram/users/views.py
+++ backend/foodgram/users/views.py
@@ -6,7 +6,7 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-from api.serializers import SubscribeSerializer, SubscribeUserSerializer
+from api.serializers import SubscribeSerializer
 from users.models import Subscribe, User
 from users.pagination import CustomPagination
 from users.serializers import CustomUserSerializer
