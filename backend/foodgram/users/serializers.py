77061dd0c2160af1b1be625e9af81cccdb480497
loren166 <107641713+loren166@users.noreply.github.com>
2023-06-05 04:03:06 +0300

    123

--- backend/foodgram/users/serializers.py
+++ backend/foodgram/users/serializers.py
@@ -9,7 +9,7 @@ class CustomUserSerializer(UserSerializer):
     is_subscribed = serializers.SerializerMethodField()
 
     def get_is_subscribed(self, obj):
-        return obj.subscribing.filter(
+        return obj.subscriber.filter(
             user=self.context.get('request').user.id
         ).exists()
 
