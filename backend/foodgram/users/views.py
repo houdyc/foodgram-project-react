7b9146adbe1e49047d415e469668ad0b2a4b431a
loren166 <107641713+loren166@users.noreply.github.com>
2023-06-06 03:14:28 +0300

    pravka

--- backend/foodgram/users/views.py
+++ backend/foodgram/users/views.py
@@ -4,6 +4,7 @@ from rest_framework import mixins, status, viewsets
 from rest_framework.decorators import action
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
+from rest_framework.views import APIView
 
 from api.serializers import SubscribeSerializer, SubscribeUserSerializer
 from users.models import Subscribe, User
