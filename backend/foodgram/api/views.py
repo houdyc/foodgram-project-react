55ed4871d891e795816dc976cb8781b2cba77fba
loren166 <107641713+loren166@users.noreply.github.com>
2023-06-06 21:37:18 +0300

    pravka

--- backend/foodgram/api/views.py
+++ backend/foodgram/api/views.py
@@ -29,6 +29,9 @@ class RecipeViewSet(viewsets.ModelViewSet):
             return RecipeSerializer
         return RecipeWriteSerializer
 
+    def get_queryset(self):
+        return Recipe.objects.filter(author=self.request.user).data
+
     def create_or_delete(self, request, pk, model, serializer, message):
         user = request.user
         recipe = get_object_or_404(Recipe, pk=pk)
