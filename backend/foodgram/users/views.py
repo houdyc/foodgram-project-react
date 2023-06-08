79625d2e80a812a1ea94a55de633878ee8267237
loren166 <107641713+loren166@users.noreply.github.com>
2023-06-08 22:41:52 +0300

    after review

--- backend/foodgram/users/views.py
+++ backend/foodgram/users/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import get_object_or_404
 from djoser.views import UserViewSet
-from rest_framework import mixins, status, viewsets
+from rest_framework import status
 from rest_framework.decorators import action
 from rest_framework.pagination import LimitOffsetPagination
 from rest_framework.permissions import IsAuthenticated
