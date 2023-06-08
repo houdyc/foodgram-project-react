2a6c477e0333cc9fbf5d295109cde2dfc5e49898
loren166 <107641713+loren166@users.noreply.github.com>
2023-06-08 07:48:25 +0300

    after review

--- backend/foodgram/api/serializers.py
+++ backend/foodgram/api/serializers.py
@@ -114,7 +114,8 @@ class RecipeWriteSerializer(serializers.ModelSerializer):
         return obj
 
     def validate_cooking_time(self, obj):
-        if obj.cooking_time <= 0:
+        recipe = obj
+        if recipe.cooking_time <= 0:
             raise serializers.ValidationError(
                 'Время приготовления должно быть больше 0!'
             )
