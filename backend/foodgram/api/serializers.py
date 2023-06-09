10497ccc993c3bc7d812ca7a216fddad1cf91edf
loren166 <107641713+loren166@users.noreply.github.com>
2023-06-09 19:59:46 +0300

    after review

--- backend/foodgram/api/serializers.py
+++ backend/foodgram/api/serializers.py
@@ -179,8 +179,7 @@ class SubscribeSerializer(serializers.ModelSerializer):
     first_name = serializers.ReadOnlyField(source='author.first_name')
     last_name = serializers.ReadOnlyField(source='author.last_name')
     is_subscribed = serializers.SerializerMethodField()
-    recipes = RecipeShortSerializer(many=True,
-                                    source='author.recipes'[:'recipes_limit'])
+    recipes = RecipeShortSerializer(many=True, source='author.recipes')
     recipes_count = serializers.SerializerMethodField()
 
     class Meta:
@@ -196,10 +195,6 @@ class SubscribeSerializer(serializers.ModelSerializer):
 
     def get_recipes_count(self, obj):
         return Recipe.objects.filter(author=obj.author).count()
-
-    def to_representation(self, instance):
-        recipe = self.get_recipes_count(Recipe)[:3]
-        return recipe
 
 
 class SubscribeUserSerializer(serializers.ModelSerializer):
