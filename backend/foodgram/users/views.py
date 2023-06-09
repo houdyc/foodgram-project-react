b42a49ba184fb71ad1630a2d0bb60aca2eff4c96
loren166 <107641713+loren166@users.noreply.github.com>
2023-06-09 03:38:29 +0300

    after review

--- backend/foodgram/users/views.py
+++ backend/foodgram/users/views.py
@@ -4,74 +4,54 @@ from rest_framework import status
 from rest_framework.decorators import action
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
-from rest_framework.views import APIView
 
-from api.serializers import SubscribeSerializer, SubscribeUserSerializer
+from api.serializers import SubscribeSerializer
 from users.models import Subscribe, User
 from users.pagination import CustomPagination
 from users.serializers import CustomUserSerializer
 
 
 class UsersViewSet(UserViewSet):
-    permission_classes = [IsAuthenticated]
-    serializer_class = CustomUserSerializer
-    pagination_class = [CustomPagination]
-
-    @action(
-        methods=['GET'],
-        detail=False,
-        permission_classes=[IsAuthenticated])
-    def me(self, request, pk=None):
-        if request.method == 'GET':
-            serializer = self.get_serializer(request.user)
-            return Response(serializer.data)
-        serializer = CustomUserSerializer(
-            request.user,
-            data=request.data,
-            partial=True)
-        serializer.is_valid(raise_exception=True)
-        return Response(serializer.data)
-
-
-class SubscribeView(APIView):
-    permission_classes = [IsAuthenticated]
-
-    def post(self, request, user_id):
-        serializer = SubscribeUserSerializer(
-            data={'user': request.user.id, 'author': user_id}
-        )
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response(data=serializer.data, status=status.HTTP_201_CREATED)
-
-    def delete(self, request, user_id):
-        follow = get_object_or_404(Subscribe, author=user_id,
-                                   user=request.user)
-        follow.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
-
-
-class SubscriptionsList(UserViewSet):
     queryset = User.objects.all()
     serializer_class = CustomUserSerializer
-    permission_classes = [IsAuthenticated]
     pagination_class = CustomPagination
 
     @action(
-        url_path='subscriptions',
-        methods=['get'],
-        detail=False,
-        permission_classes=(IsAuthenticated,),
+        detail=False, methods=["get"], permission_classes=[IsAuthenticated]
     )
-    def get_subscriptions(self, response):
-        limit = self.request.query_params.get('recipes_limit')
-        pages = self.paginate_queryset(
-            User.objects.filter(author__user=self.request.user)
+    def subscriptions(self, request):
+        user = request.user
+        queryset = User.objects.filter(following__user=user)
+        page = self.paginate_queryset(queryset)
+        serializer = SubscribeSerializer(
+            page, many=True, context={"request": request}
         )
-        serializer = SubscribeSerializer(pages, many=True)
-        if limit:
-            for user in serializer.data:
-                if user.get('recipes'):
-                    user['recipes'] = user.get('recipes')[:int(limit)]
-
         return self.get_paginated_response(serializer.data)
+
+    @action(
+        detail=True,
+        methods=["post", "delete"],
+        permission_classes=[IsAuthenticated],
+    )
+    def subscribe(self, request, id):
+        user = request.user
+        author = get_object_or_404(User, id=id)
+
+        if request.method == "POST":
+            Subscribe.objects.create(user=user, author=author)
+            serializer = SubscribeSerializer(
+                author, context={"request": request}
+            )
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        if not Subscribe.objects.filter(
+            user=user, author=author
+        ).exists():
+            return Response(
+                {"errors": "Вы не подписаны"},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+        subscription = get_object_or_404(
+            Subscribe, user=user, author=author
+        )
+        subscription.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
