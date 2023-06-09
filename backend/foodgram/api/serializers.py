0b2908cc392e2eb784810d377343a1305f6a392a
loren166 <107641713+loren166@users.noreply.github.com>
2023-06-09 19:27:17 +0300

    after review

--- backend/foodgram/api/serializers.py
+++ backend/foodgram/api/serializers.py
@@ -195,7 +195,7 @@ class SubscribeSerializer(serializers.ModelSerializer):
                                         ).exists()
 
     def get_recipes_count(self, obj):
-        return Recipe.objects.filter(author=obj.author).count()[:3]
+        return Recipe.objects.filter(author=obj.author).count()
 
 
 class SubscribeUserSerializer(serializers.ModelSerializer):
